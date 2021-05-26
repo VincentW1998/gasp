58b90a804c54ad2427b5c91b7ec52cacc24812e8
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2021-05-26 18:21:00 +0200

    affichage bizzare

--- turtle.ml
+++ turtle.ml
@@ -3,15 +3,16 @@ open Graphics
 let pen_position = ref true
 let isUp () = pen_position := false
 let isDown () = pen_position := true
-let x = ref 200.
-let y = ref 200.
-let angle = ref 90.
+let x = ref 0.
+let y = ref 0.
 
 (** constant pi **)
 let pi = 4. *. atan(1.)
 
 (** function change degree to rad**)
 let degreeToRad = (fun x -> x *. (pi /. 180.))
+
+let angle = ref (degreeToRad 90.)
 
 (** get the sign of x and add this sign to 0.5**)
 let getSign = (fun x -> if x < 0. then (-0.5) else 0.5)
@@ -22,11 +23,11 @@ let roundFloat x =
 
 (* Polar cordinate to cartesian for axe X *)
 let cordinateX length =
-  float_of_int(length)  *. cos (degreeToRad !angle)
+  float_of_int(length)  *. cos (!angle)
 
 (* Polar cordinate to cartesian for axe Y *)
 let cordinateY length =
-  float_of_int(length) *. sin (degreeToRad !angle)
+  float_of_int(length) *. sin (!angle)
 
 let tourne a = angle := !angle +. degreeToRad (float_of_int a)
 
